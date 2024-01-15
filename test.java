d282007aa6d226256e6cfc5c8b4f374d54ffa812
楠zz <1204484155@qq.com>
2024-01-15 16:41:57 +0800

    master （the first）

--- test.java
+++ test.java
@@ -1,11 +1,16 @@
 /**
  * @author HAO
  * @description 功能描述
+<<<<<<< HEAD
  * @create 2024/1/15 16:42
+=======
+ * @create 2024/1/15 16:40
+>>>>>>> master （the first）
  * @Version：V1.0
  */
 public class test {
     public static void main(String[] args) {
+        System.out.println("这是master分支！");
         System.out.println("这是hot-fix分支");
         System.out.println("2");
     }
