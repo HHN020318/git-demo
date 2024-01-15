085f4b84682f23e3e44072c08ba97b5f1639d6a4
楠zz <1204484155@qq.com>
2024-01-15 16:41:57 +0800

    master （the first）

---
+++ test.java
@@ -0,0 +1,11 @@
+/**
+ * @author HAO
+ * @description 功能描述
+ * @create 2024/1/15 16:40
+ * @Version：V1.0
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("这是master分支！");
+    }
+}
